9e24643ba5e262a1a3627726824cf9dac6032322
Vasyl Miskiv <vasylmiskiv@gmail.com>
2023-03-31 21:00:14 +0300

    ahve been added posts widget and displaying friends

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,17 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlines,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  Typography,
+  useTheme,
+} from "@mui/icons-material";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
